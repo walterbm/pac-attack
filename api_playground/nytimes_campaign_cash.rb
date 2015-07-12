09361c964148276d2217c27bbcdcc246786b869f
walterbm <walter.beller.morales@gmail.com>
2015-07-12 16:22:46 -0400

    add api playground

--- api_playground/nytimes_campaign_cash.rb
+++ api_playground/nytimes_campaign_cash.rb
@@ -1,8 +1,11 @@
 require 'rubygems'
 require 'campaign_cash'
-require 'pry'
 include CampaignCash
 
-Base.api_key = "f00fe1ee06f70d27bd59eca0c5382c5f:10:72490814"
+require 'pry'
+
+require_relative 'api_playground/keys'
+
+Base.api_key = API_KEY['NYT']
 binding.pry
 Candidate.search("Bass", 2012).first.name
